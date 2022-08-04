767e209c993bf1e337acb84657d4720fab46a1dc
Richard Bates <rich.bates@protonmail.com>
2022-08-04 11:52:12 +0800

    Adds sumAll functionality

--- sum/sum.go
+++ sum/sum.go
@@ -1,11 +1,19 @@
 package sum
 
-func Sum(numbers [3]int) int {
-	var sum int
-
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
 
-	return sum
+	return
+}
+
+func SumAll(numberArrays ...[]int) []int {
+	sums := make([]int, len(numberArrays))
+
+	for i, numberArray := range numberArrays {
+		sums[i] = Sum(numberArray)
+	}
+
+	return sums
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,20 +1,38 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	array := [3]int{2, 2, 1}
-	sum := Sum(array)
-	expected := 5
 
-	if sum != expected {
-		t.Errorf("expected %q, but got %q", expected, sum)
-	}
+	t.Run("when passing collection of any size", func(t *testing.T) {
+		array := []int{2, 2, 1, 4, 5}
+		sum := Sum(array)
+		expected := 14
+
+		if sum != expected {
+			t.Errorf("expected %q, but got %q", expected, sum)
+		}
+	})
 }
 
 func BenchmarkSum(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Sum([3]int{1, 2, 3})
+		Sum([]int{1, 2, 3})
 	}
 	// Expected output 6
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("when passing multiple slices", func(t *testing.T) {
+		sum := SumAll([]int{2, 2}, []int{1, 1})
+		expected := []int{4, 2}
+
+		if !reflect.DeepEqual(sum, expected) {
+			t.Errorf("expected %v, but got %v", expected, sum)
+		}
+	})
 }
