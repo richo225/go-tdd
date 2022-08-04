a66dc3a1cf52607c8657547c9f0347f0c6447910
Richard Bates <rich.bates@protonmail.com>
2022-08-04 10:24:43 +0800

    Refactor sum with for range

--- sum/sum.go
+++ sum/sum.go
@@ -3,8 +3,8 @@ package sum
 func Sum(numbers [3]int) int {
 	var sum int
 
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
